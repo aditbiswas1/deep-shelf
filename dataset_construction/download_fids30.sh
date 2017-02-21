48e1b144cd28c065e27f0b9c51a8924906bc4a82
Adit Biswas <adit.biswas@grofers.com>
2017-02-21 23:54:50 +0530

    add fids30 dataset

---
+++ dataset_construction/download_fids30.sh
@@ -0,0 +1,3 @@
+wget http://data.vicos.si/datasets/FIDS30/FIDS30.zip
+unzip FIDS30.zip
+rm FIDS30.zip
---
+++ dataset_construction/fids30.py
@@ -0,0 +1,49 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import pandas as pd
+import numpy as np
+import glob
+from matplotlib import pyplot
+
+
+# In[2]:
+
+labels = []
+image_files = []
+
+
+# In[9]:
+
+folders = glob.glob('FIDS30/*')
+
+
+# In[15]:
+
+for i,f in enumerate(folders):
+    fruit = not f.split('.')[-1] == 'txt'
+    if fruit:
+        label = 'fid'+str(i)
+        f_glob = glob.glob(f+'/*')
+        for image in f_glob:
+            labels.append(label)
+            image_files.append(image)
+        
+
+
+# In[19]:
+
+df = pd.DataFrame({'label' : labels, 'file': image_files})
+
+
+# In[21]:
+
+df.to_csv('fids30.csv')
+
+
+# In[ ]:
+
+
+
